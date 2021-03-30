2a9f7f58305d6dc6a935cf96cb0763a55208cd79
Edchel Stephen <edchelstephens@gmail.com>
2021-03-30 13:22:39 +0800

    news app views.

--- django_overview/news/views.py
+++ django_overview/news/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from news.models import Article
+
+from utils.debug_utils import *
+
+def year_archive(request, year):
+    """Render articles on a specified year."""
+    articles = Article.objects.filter(pub_date__year=year)
+    context = { 'year': year, 'articles': articles }
+    return render(request, 'news/year_archive.html', context)
+
+def month_archive(request, year, month):
+    """Render articles on a specified month on a specific year."""
+    articles = Article.objects.filter(pub_date__year=year, pub_date__month=month)
+    context = { 'year': year, 'month': month, 'articles': articles }
+    pprint_local_vars(locals())
+    return render(request, 'news/month_archive.html', context)
+
+def article_detail(request, pk):
+    """Render article detail."""
+    article = Article.objects.get(pk=pk)
+    context = { 'pk':pk, 'article': article }
+    return render(request, 'news/article_detail.html', context)
